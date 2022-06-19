b42b34b4ba27110bb8a30a411e90b010ad94cd55
Marcos Danilo <marcosdanilo94@gmail.com>
2022-06-19 20:01:06 -0300

    feat: Requisito 4 ok - Adiciona as validacoes para o endpoint /login

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,9 +1,22 @@
 const generateToken = require('./generateToken');
 
+const INVALID_EMAIL_MESSAGE = 'O "email" deve ter o formato "email@email.com"';
+const INVALID_PASSWORD_MESSAGE = 'O "password" deve ter pelo menos 6 caracteres';
+
+const validateEmail = (email) => {
+  const validEmail = /\S+@\S+\.\S+/;
+  const emailTest = validEmail.test(email);
+  return emailTest;
+};
+
 const authMiddleware = (req, res, next) => {
   const { email, password } = req.body;
+  const emailTest = validateEmail(email);
 
-  if (!email || !password) return res.status(400).end();
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) return res.status(400).json({ message: INVALID_PASSWORD_MESSAGE });
+  if (!emailTest) return res.status(400).json({ message: INVALID_EMAIL_MESSAGE });
 
   const token = generateToken();
   
