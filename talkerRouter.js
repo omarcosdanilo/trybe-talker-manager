a5b26e279a44a4a980e69779b7b563b22b0ac051
Marcos Danilo <marcosdanilo94@gmail.com>
2022-06-19 19:00:30 -0300

    feat: Requisito 2 ok - Cria o endpoint GET /talker/:id e retorna um resultado com base no id da rota

--- talkerRouter.js
+++ talkerRouter.js
@@ -4,9 +4,25 @@ const fs = require('fs/promises');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+const getTalkers = async () => {
   const talkers = await fs.readFile('./talker.json', 'utf-8');
-  return res.status(200).json(JSON.parse(talkers));
+  const talkersParsed = JSON.parse(talkers);
+  return talkersParsed;
+};
+
+router.get('/', async (req, res) => {
+  const talkers = await getTalkers();
+  return res.status(200).json(talkers);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await getTalkers();
+  const talkerIndex = talkers.findIndex((talker) => talker.id === Number(id));
+
+  if (talkerIndex !== -1) return res.status(200).json(talkers[talkerIndex]);
+
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = router;
